7571fa4ae2d54953791d6819dfcc628825a256d9
“Rocio <dev.alvarezbernal@gmail.com>
2020-04-28 20:34:01 -0500

    added divs no css

--- group5/src/component/aFrameFor.jsx
+++ group5/src/component/aFrameFor.jsx
@@ -12,6 +12,16 @@ render(
         <div className="overall-page">
             {/* <nav>NAVIGATION BAR COMMING SOON</nav> */}
 
+            <div className="left">
+                <article>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </article>
+
+                <div>
+                    <img className="image" src='#'> </img>
+                </div>
+            </div>
+
         </div>
 )
 
