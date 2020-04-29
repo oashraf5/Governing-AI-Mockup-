df02931dde09594cd996086e53651a6bcda08a4a
“Rocio <dev.alvarezbernal@gmail.com>
2020-04-29 09:13:49 -0500

    removed page from displaying on app.js

--- group5/src/App.js
+++ group5/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Aframefor from './component/Aframefor'
+// import Aframefor from './component/Aframefor' this page needs to ne linked instead of rendered on the landing page
 
 function App() {
   return (
     <div className="App">
 
-      <Aframefor />
+     
 
     </div>
   );
