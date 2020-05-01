a99b46aa7aa118d22db5c9b5af1a3a5a3a3ae40e
Briandepalmas <Student_GA110@Student-GA110.home>
2020-05-01 15:27:11 -0400

    added stakeholder component

---
+++ group5/src/component/stakeholders/Stakeholders.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+// import {Route,Switch, Link , BrowserRouter as Router} from 'react-router-dom';
+import '../App.css'
+export class Stakeholders extends Component {
+    render() {
+        return (
+            
+                <div className="bubbles">
+                    <div id="first-bubble"></div>
+                </div>
+                      
+        )
+    }
+}
+
+export default Stakeholders
