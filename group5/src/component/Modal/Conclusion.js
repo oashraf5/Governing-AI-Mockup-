1b553a2f26c5d7e86b5e663f93394a9ae618d130
Visa <visalakshi.cse@gmail.com>
2020-05-02 13:17:04 -0500

    Conclusion content writing completed

--- group5/src/component/Modal/Conclusion.js
+++ group5/src/component/Modal/Conclusion.js
@@ -37,6 +37,20 @@ TO DISREGARD HUMAN RIGHTS WOULD BE TO CLOSE OFF THAT PATH
         operationalize human rights into business models,workflows and product design
     </p>
     </li>
+    <li><p> Academics should further examine the value ,limations, and interactions between human
+        rights law and human dignity approaches;humanitarian law;and ethics in relation to emerging
+        AI technologies.Human rights and legal scholars should work with the other stakeholders on the tradeoffs between rights when
+        faced with specific.AI risks and harms.Social science researchers should emprically investigate the on-the-ground impact of AI on human rights
+    </p>
+    </li>
+    <li><p> UN human rights investigators and special reporters should continue researching
+        and publicizing the human rights impacts resulting from AI systems.UN officials and
+        participating governments should evaluate whether existing UN mechanisms for international 
+        rights monitoring,accountability, and redress are adequate to respond to AI and other
+        rapidly emerging technologies.UN leadership should also assume a central role in international
+        technology debates by promoting shared global values based on fundamental rights and human dignity
+    </p>
+    </li>
 
 </ul>
 </div>
