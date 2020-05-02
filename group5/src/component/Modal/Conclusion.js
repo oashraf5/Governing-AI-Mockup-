1856101a5c46616b9c6814e3648d778caafa1913
Visa <visalakshi.cse@gmail.com>
2020-05-02 00:42:37 -0500

    Added hr and p tag

--- group5/src/component/Modal/Conclusion.js
+++ group5/src/component/Modal/Conclusion.js
@@ -4,14 +4,31 @@ import ReactPlayer from "react-player"
 
 function Conclusion() {
   return (
-    <div className="video">
+    <div className="wrapper">
+        
+        <div className="hr">
+        <h1>Introduction</h1>
+          
+<hr/>
 
+<p> 
+In order for AI to benefit the common good,
+at the very least its design and deployment 
+should avoid harms to fundamental
+human values.International human rights 
+provides a robustand global formulation of
+ those values
+    
+ </p>   
+<hr />
+</div>
+<div className="video">
 <ReactPlayer
         url="https://www.youtube.com/watch?v=ErIEjpvAgck"
         width='100%'
           height='150%'
       />
-     
+     </div>
 
 
     </div>
