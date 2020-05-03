475c158c45acaf80e9e1cc89124272eb35ce1d35
“Rocio <dev.alvarezbernal@gmail.com>
2020-05-03 01:16:42 -0500

    added css

--- group5/src/App.js
+++ group5/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
+import Aframefor from './component/aFrameFor';
 // import Aframefor from './component/Aframefor' this page needs to ne linked instead of rendered on the landing page
 
 function App() {
   return (
     <div className="App">
-
+<Aframefor />
      
-
     </div>
   );
 }
--- group5/src/component/aFrameFor.jsx
+++ group5/src/component/aFrameFor.jsx
@@ -14,14 +14,31 @@ render() {
             {/* <nav>NAVIGATION BAR COMMING SOON</nav> */}
 
             <div className="left">
-                <article>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <h2>A HUMAN RIGHTS FRAME FOR AI RISKS AND HARMS</h2>
+                <article className="scroll">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae congue eu consequat ac felis donec. Sagittis orci a scelerisque purus semper eget duis. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Massa sapien faucibus et molestie. Est ante in nibh mauris. Non nisi est sit amet facilisis magna etiam tempor orci. Sagittis id consectetur purus ut faucibus pulvinar elementum integer. Elementum eu facilisis sed odio morbi quis commodo. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida. Amet dictum sit amet justo. Feugiat vivamus at augue eget arcu dictum varius duis. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Ut pharetra sit amet aliquam id. Ut placerat orci nulla pellentesque dignissim enim sit. Vestibulum sed arcu non odio euismod lacinia at. Interdum velit laoreet id donec.
+                <br/>
+                <br/>
+                Massa tempor nec feugiat nisl pretium fusce. Scelerisque felis imperdiet proin fermentum leo vel orci. Ultrices in iaculis nunc sed augue lacus. Et odio pellentesque diam volutpat commodo sed egestas. Eros donec ac odio tempor. In pellentesque massa placerat duis ultricies. Lectus proin nibh nisl condimentum id. Pellentesque sit amet porttitor eget dolor morbi. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl vel. Feugiat pretium nibh ipsum consequat nisl vel pretium. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Nulla pellentesque dignissim enim sit amet venenatis urna. Elit at imperdiet dui accumsan.
+                <br/>
+                <br/>
+                In metus vulputate eu scelerisque. Vitae elementum curabitur vitae nunc sed velit dignissim. Arcu bibendum at varius vel pharetra vel. In fermentum et sollicitudin ac orci phasellus egestas. A cras semper auctor neque vitae. Tellus at urna condimentum mattis pellentesque id nibh tortor. Sagittis eu volutpat odio facilisis mauris. Nisi scelerisque eu ultrices vitae auctor. Massa tincidunt dui ut ornare lectus sit amet est placerat. Et ligula ullamcorper malesuada proin libero. Pulvinar etiam non quam lacus. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Auctor eu augue ut lectus. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque.
+                <br/>
+                <br/>
+                Aliquam ultrices sagittis orci a. Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. Semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Viverra mauris in aliquam sem fringilla ut morbi tincidunt. Mus mauris vitae ultricies leo integer. Sit amet aliquam id diam maecenas. Vestibulum lectus mauris ultrices eros in cursus. Odio eu feugiat pretium nibh ipsum. Tincidunt id aliquet risus feugiat in ante metus. Eros in cursus turpis massa tincidunt dui ut ornare.
+                <br/>
+                <br/>
+                Congue eu consequat ac felis. Curabitur vitae nunc sed velit dignissim sodales ut eu. Id diam maecenas ultricies mi eget. Et leo duis ut diam quam nulla porttitor massa. Amet consectetur adipiscing elit pellentesque habitant morbi. Nulla facilisi nullam vehicula ipsum a arcu. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Volutpat consequat mauris nunc congue nisi vitae suscipit. Velit scelerisque in dictum non consectetur a erat nam. Amet consectetur adipiscing elit ut aliquam purus sit amet.
                 </article>
-
-                <div>
-                    <img className="image" src='#'/>
                 </div>
-            </div>
+
+                <div className="image">
+                    <img  src='#'/>
+                </div>
+
+                <form id="comment-section">
+
+                </form>
 
         </div>
     );
